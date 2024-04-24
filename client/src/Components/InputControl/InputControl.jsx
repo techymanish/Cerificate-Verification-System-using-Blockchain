d0496beee03292e85ecdec40bea0a149a31d9b46
Mohak Agarwal <agarwalmohak6@gmail.com>
2024-04-25 00:16:26 +0530

    init

--- client/src/Components/InputControl/InputControl.jsx
+++ client/src/Components/InputControl/InputControl.jsx
@@ -3,7 +3,7 @@ import styles from "./InputControl.module.css";
 function InputControl(props) {
   return (
     <div className={styles.container}>
-      {/* {props.label && <label>{props.label}</label>} */}
+      props.label && <label>props.label</label>
       <input type="text" {...props} />
     </div>
   );
